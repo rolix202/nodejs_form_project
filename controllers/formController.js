fb16be934a3df7a68da1077cdea0fbc49837b8fc
rolix202 <williamsroland147@gmail.com>
2024-06-26 00:34:59 +0100

    added the success page and error notification

--- controllers/formController.js
+++ controllers/formController.js
@@ -1,14 +1,57 @@
-
-// This is where you process your form inputs
+import { body, validationResult } from "express-validator"
 
 export const getForm = (req, res, next) => {
     res.render("form")
 }
 
-export const postForm = (req, res, next) => {
-    
-}
+export const postForm = [
+    body("first_name")
+        .notEmpty()
+        .withMessage("First name required!")
+        .trim()
+        .escape(),
+    body("last_name")
+        .notEmpty()
+        .withMessage("Last name is required!")
+        .trim()
+        .escape(),
+    body("email")
+        .isEmail()
+        .withMessage("Not a valid Email address!")
+        .trim()
+        .escape(),
+    body("password")
+        .isLength({min: 4})
+        .withMessage("Password must be at least 4 characters long!")
+        .trim()
+        .escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req)
+
+        if(!errors.isEmpty()){
+            return res.render("form", {errors: errors.array()})
+        }
+
+        const {first_name, last_name, email, password} = req.body
+
+        const user = {
+            firstName: first_name,
+            lastName: last_name,
+            email: email,
+            password: password
+        }
+
+        // Store user data in session
+        req.session.user = user;
+
+        res.redirect("/success");
+}]
 
 export const getSuccess = (req, res, next) => {
-   
+    // Retrieve user data from session
+    const user = req.session.user;
+
+    // Render success page with user details
+    res.render("success", { user });
 }
--- index.js
+++ index.js
@@ -2,8 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import formRoute from "./routes/formRoute.js"
+import session from "express-session";
 
 const app = express()
+
+// Configure the session middleware
+const sessionMiddleware = session({
+    secret: 'your_secret_key', // Replace with any strong secret key of your choice
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+})
+
+app.use(sessionMiddleware)
 
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}))
